8e7ed945ed63f405e3e7b6cb88156266ac144c4a
hairethan12 <153117354+hairethan12@users.noreply.github.com>
2025-02-07 17:42:51 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCMission5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
